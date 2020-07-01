6a03b8c68a1b4e40b2f7326cd3b37a713dc863d6
Dylan <dylanabozarth@gmail.com>
2020-07-01 12:35:08 -0700

    FCC copy

--- src/pages/productpages/neolithic.js
+++ src/pages/productpages/neolithic.js
@@ -1,65 +1,67 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { DisplayItem } from '../itemdisplayer/itemdisplayer'
+
 
 class Neolithic extends Component {
-  constructor() {
-    super();
-    this.state = {
-      items: [
+    constructor() {
+        super();
+    this.state = { items: [
         {
-          title: "Neolithic Rock art",
-          imageUrl:
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.appstate.edu%2F_images%2Finfo-icon.png&f=1&nofb=1",
-          price: "$300",
+          title: 'Neolithic Rock art',
+          imageUrl: "./itemphotos/stoneage/art.jpg",
+          price: '$300',
           id: 1,
-          desc: "Own a (reproduction) priceless piece of human history. ",
+          desc: "Own a (reproduction) priceless piece of human history. "
         },
         {
-          title: "Neolithic axe",
-          imageUrl:
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.appstate.edu%2F_images%2Finfo-icon.png&f=1&nofb=1",
-          price: "$10",
+          title: 'Neolithic axe',
+          imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.6VlNGksN5K9h8VpDxMsH5gHaLI%26pid%3DApi&f=1',
+          price: '$10',
           id: 2,
-          desc: '"Ung like, rock hard." - A satisfied customer"',
+          desc: '"Ung like, rock hard." - A satisfied customer"'
         },
         {
-          title: "Neolithic Spear",
-          imageUrl:
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.appstate.edu%2F_images%2Finfo-icon.png&f=1&nofb=1",
-          price: "$15",
-          id: 3,
-          desc: "A rock on the end of a stick, Classic.",
+          title: 'Neolithic Spear',
+          imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.6VlNGksN5K9h8VpDxMsH5gHaLI%26pid%3DApi&f=1',
+          price: '$15',
+          id: 5,
+          desc: 'A rock on the end of a stick, Classic.'
         },
         {
-          title: "Neolithic Dagger",
-          imageUrl:
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.appstate.edu%2F_images%2Finfo-icon.png&f=1&nofb=1",
-          price: "$5",
+          title: 'Neolithic Dagger',
+          imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.6VlNGksN5K9h8VpDxMsH5gHaLI%26pid%3DApi&f=1',
+          price: '$5',
           id: 3,
-          desc: "Just in case you lose your spear or axe",
+          desc: 'Just in case you lose your spear or axe'
         },
         {
-          title: "Book about the Stone age",
-          imageUrl:
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.appstate.edu%2F_images%2Finfo-icon.png&f=1&nofb=1",
-          price: "$15",
-          id: 3,
-          desc:
-            "Unlike stone age people, you can read! So you should read about stone age people, because they cannot read about you. ",
-        },
-      ],
-    };
+          title: 'Book about the Stone age',
+          imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.6VlNGksN5K9h8VpDxMsH5gHaLI%26pid%3DApi&f=1',
+          price: '$15',
+          id: 4,
+          desc: 'Unlike stone age people, you can read! So you should read about stone age people, because they cannot read about you. '
+        }
+      ] 
+    }
   }
-  render() {
-    var displayitems = this.state.items.map(
-      ({ title, imageUrl, desc, price, id }) => (
-        <div key={id} className="itemlist row">
-          {" "}
-          <h1>{title}</h1> <img className="" width="200px" height="200px" src={imageUrl} /> <p>{desc} </p> <p>{price}</p>
-        </div>
-      )
-    );
-    return <div className="container">{displayitems} </div>;
-  }
-}
-
+    render() { 
+        return ( 
+        <div className="FullPage Container"> 
+        <div className="NeolithicInfo">
+        <h1>The Neolithic age, also known as the stone age. Was the age where humans used stone as the primary material for all technology.</h1>
+        <div className="ItemList row">
+        {
+             
+            this.state.items.map(({title, imageUrl, desc, price, id }) => (
+            <DisplayItem key={id} title={title} imageUrl={imageUrl} desc={desc} price={price} />
+        ))} 
+            </div> 
+            
+            </div>
+            
+            </div>
+        )
+    }
+} 
+        
 export default Neolithic;
