3d51e9b9a8fe1379e64d5abfcca914745a0ec573
Dylan <dylanabozarth@gmail.com>
2020-07-09 19:56:54 -0700

    see if that worked

---
+++ build/precache-manifest.c90c7476270e00ce4057dae2e08372c5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de7ea69332ccd8d04147963cc35c089b",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/index.html"
+  },
+  {
+    "revision": "ad11a042b0c17bfa77e2",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "947a7561da78885602c1",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/css/main.fae0402b.chunk.css"
+  },
+  {
+    "revision": "ad11a042b0c17bfa77e2",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/js/2.062369cb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/js/2.062369cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947a7561da78885602c1",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/js/main.ee384bc0.chunk.js"
+  },
+  {
+    "revision": "f0647a26994c41170863",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/js/runtime-main.15005078.js"
+  },
+  {
+    "revision": "b5eb6d9e5a4eca687ef0c58fff6a1dc5",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/media/archery.b5eb6d9e.jpg"
+  },
+  {
+    "revision": "373be079bdc24b7bdf2189472d309e0b",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/media/armor.373be079.jpg"
+  },
+  {
+    "revision": "3f7d24490b7d5f16fd153b372e13a4f2",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/media/banner.3f7d2449.jpg"
+  },
+  {
+    "revision": "6a409549c5c247c8fe2d35e5d18bdaa9",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/media/bronze.6a409549.jpg"
+  },
+  {
+    "revision": "7abda0dac26a407f8eae7a9ef2ba6e18",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/media/iron.7abda0da.jpg"
+  },
+  {
+    "revision": "c726e77dd54664b3ed5d12069ce7f216",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/media/medieval.c726e77d.jpg"
+  },
+  {
+    "revision": "e3654da76bb888ae2e05f46396f0b384",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/media/neolithic.e3654da7.jpg"
+  },
+  {
+    "revision": "7672eae6cb6c2695f600ffaf45e74fe8",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/media/paper.7672eae6.jpg"
+  },
+  {
+    "revision": "c6eb9a3f82d0257950f0dc604ab2af64",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/media/renaissance.c6eb9a3f.jpg"
+  },
+  {
+    "revision": "d4e199174c19d4dc9de5b632be6c8a9d",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/media/roman.d4e19917.jpg"
+  },
+  {
+    "revision": "3aadc8d908b9e7ce8cd14b5f00127e4c",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/media/shields.3aadc8d9.jpg"
+  },
+  {
+    "revision": "a31abaa1a1df8f147839e6d4f530e1e9",
+    "url": "/Imstupidpleasehelp/Historical-weapons-shop/static/media/swords.a31abaa1.jpg"
+  }
+]);
